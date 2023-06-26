532f5304a9df3ff841498401ec5489f6b06bd2a8
kweeka <mtalbi29@yahoo.com>
2023-06-26 17:07:46 +0100

    Add header layout

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { ChevronsRight, Unlock, ChevronDown } from 'lucide-react'
+import BreadCrumb from '../components/BreadCrumb'
+import Seperator from '../components/Seperator'
+
+const Header = ({ breadCrumbs }) => {
+  return (
+    <header className='w-full h-16 flex justify-between p-2'>
+      <section className='flex items-center gap-2'>
+        <div className='text-slate-600 px-4'>
+          <ChevronsRight size={18} />
+        </div>
+        <div className='flex gap-2'>
+          {breadCrumbs.map((crumb, index) => (
+            index !== breadCrumbs.length - 1
+            ? (
+              <BreadCrumb slash={true} key={index}>
+                {crumb}
+              </BreadCrumb>
+            ) : (
+              <BreadCrumb slash={false} key={index}>
+                {crumb}
+              </BreadCrumb>
+            )
+          ))}
+        </div>
+      </section>
+      <section className='flex items-center gap-3'>
+        <div className='text-slate-400 inline-flex items-center'>
+          <Unlock size={18} />
+          <span className='ml-1'>Editing</span>
+        </div>
+        <Seperator className={"h-5"} vertical={true} />
+        <div className='text-blue-600 font-semibold inline-flex items-center'>
+          <span>Publish Space</span>
+          <ChevronDown className='pt-1' size={22} />
+        </div>
+      </section>
+    </header>
+  )
+}
+
+export default Header

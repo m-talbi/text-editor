8609fad4ef100ca6386f5e845d95c84d5c35e0b1
kweeka <mtalbi29@yahoo.com>
2023-07-01 14:00:27 +0100

    Update Toolbar

--- src/layouts/Toolbar.jsx
+++ src/layouts/Toolbar.jsx
@@ -6,8 +6,8 @@ const Toolbar = ({ username, userIcon, readTime, viewCount }) => {
   return (
     <div className="py-[0.4rem] px-2 border border-slate-200 rounded-md shadow-sm flex items-center justify-between">
       <section className="flex items-center gap-2">
-        <div className="bg-green-200 rounded-md px-[0.35rem] flex items-center justify-center">
-          <span className="text-green-800 text-lg font-bold p-1 h-fit">{username[0].toUpperCase()}</span>
+        <div className="bg-green-200 rounded-[4px] px-[0.35rem] flex items-center justify-center">
+          <span className="text-green-800 text-lg font-bold p-[0.15rem] h-fit">{username[0].toUpperCase()}</span>
         </div>
         <Seperator className={"h-4 bg-slate-400"} vertical={true} />
         <div className="inline-flex items-center gap-2 text-slate-400">
@@ -20,7 +20,7 @@ const Toolbar = ({ username, userIcon, readTime, viewCount }) => {
         </div>
         <Seperator className={"h-4 bg-slate-400"} vertical={true} />
         <div className="text-slate-400 flex items-center">
-          <ArrowDownLeft size={24} className="pt-1" />
+          <ArrowDownLeft size={20} />
           <span>{viewCount}</span>
         </div>
       </section>

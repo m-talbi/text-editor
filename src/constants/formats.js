34e552bef8b358235a1a44a7d20ab9479d3363c5
kweeka <mtalbi29@yahoo.com>
2023-07-06 21:00:42 +0100

    Add formats.js

---
+++ src/constants/formats.js
@@ -0,0 +1,10 @@
+const textformats = [
+  {format: "Heading 1", shortcut: "# + space"},
+  {format: "Heading 2", shortcut: "## + space"},
+  {format: "Heading 3", shortcut: "### + space"},
+  {format: "Heading 4", shortcut: "#### + space"},
+  {format: "Heading 5", shortcut: "##### + space"},
+  {format: "Heading 6", shortcut: "###### + space"},
+];
+
+export default textformats;

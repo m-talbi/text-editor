755868b3376c6d656a863d4d4c216467cedf8f24
kweeka <mtalbi29@yahoo.com>
2023-07-09 17:30:26 +0100

    Update formats

--- src/constants/formats.js
+++ src/constants/formats.js
@@ -1,10 +1,11 @@
-const textformats = [
+const formats = [
   {format: "Heading 1", shortcut: "# + space"},
   {format: "Heading 2", shortcut: "## + space"},
   {format: "Heading 3", shortcut: "### + space"},
   {format: "Heading 4", shortcut: "#### + space"},
   {format: "Heading 5", shortcut: "##### + space"},
   {format: "Heading 6", shortcut: "###### + space"},
+  {format: "Bold", shortcut: "** + space"},
 ];
 
-export default textformats;
+export default formats;

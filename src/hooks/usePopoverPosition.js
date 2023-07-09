9461ef93e29bcaa107b1ccf18b00c334f55fbe72
kweeka <mtalbi29@yahoo.com>
2023-07-09 17:30:38 +0100

    Add usePopoverPosition hook

---
+++ src/hooks/usePopoverPosition.js
@@ -0,0 +1,33 @@
+import { useLayoutEffect, useRef } from "react";
+
+const usePopoverPosition = (editorEl, { x, y }) => {
+  const popoverRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (!popoverRef.current || editorEl !== document.activeElement) return;
+
+    const movePopoverToCaret = () => {
+      popoverRef.current.style.top = `${top}px`;
+      if (editorEl.offsetWidth - left > popoverRef.current.offsetWidth) {
+        popoverRef.current.style.left = `${left}px`;
+      }
+    };
+
+    const popoverWidth = popoverRef.current.offsetWidth;
+    const popoverHeight = popoverRef.current.offsetHeight;
+    const editorWidth = editorEl.offsetWidth;
+    const editorHeight = editorEl.offsetHeight;
+
+    const popoverLeft = x - popoverWidth / 2 - 10;
+    const popoverTop = y - popoverHeight / 2;
+
+    const left = Math.max(0, Math.min(popoverLeft, editorWidth - popoverWidth));
+    const top = Math.max(25, Math.min(popoverTop, editorHeight - popoverHeight));
+
+    movePopoverToCaret();
+  }, [editorEl, x, y]);
+
+  return popoverRef;
+};
+
+export default usePopoverPosition;

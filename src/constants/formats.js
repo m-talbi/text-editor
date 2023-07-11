f81945caec11910dadc8a99274147d27ba97e80c
kweeka <mtalbi29@yahoo.com>
2023-07-11 20:17:45 +0100

    Update formats

--- src/constants/formats.js
+++ src/constants/formats.js
@@ -1,11 +1,11 @@
 const formats = [
-  {format: "Heading 1", shortcut: "# + space"},
-  {format: "Heading 2", shortcut: "## + space"},
-  {format: "Heading 3", shortcut: "### + space"},
-  {format: "Heading 4", shortcut: "#### + space"},
-  {format: "Heading 5", shortcut: "##### + space"},
-  {format: "Heading 6", shortcut: "###### + space"},
-  {format: "Bold", shortcut: "** + space"},
+  {format: "Heading 1", tag: "h1", shortcut: "# + space"},
+  {format: "Heading 2", tag: "h2", shortcut: "## + space"},
+  {format: "Heading 3", tag: "h3", shortcut: "### + space"},
+  {format: "Heading 4", tag: "h4", shortcut: "#### + space"},
+  {format: "Heading 5", tag: "h5", shortcut: "##### + space"},
+  {format: "Heading 6", tag: "h6", shortcut: "###### + space"},
+  {format: "Bold", tag: "b", shortcut: "** + space"},
 ];
 
 export default formats;

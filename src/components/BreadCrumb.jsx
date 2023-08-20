fa0d42abb6211f91609857b63bdaa3c95127406b
kweeka <mtalbi29@yahoo.com>
2023-08-20 16:58:59 +0100

    Add dark mode

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const BreadCrumb = ({ children, slash }) => {
   return (
-    <div className="inline-flex items-center text-slate-400">
+    <div className="inline-flex items-center text-slate-400 dark:text-[#deddda]">
       {children}
       {slash && <span className="text-xl ml-2">  /</span> }
     </div>
--- src/components/PopoverList.jsx
+++ src/components/PopoverList.jsx
@@ -1,39 +1,34 @@
 /* eslint-disable react/prop-types */
-import { useRef, useEffect } from "react";
 import { Pencil } from "lucide-react"
 import { twMerge } from "tailwind-merge";
-import {  } from '../utils/getCaretRect';
+import { useScrollIntoItem } from "../hooks/useScrollIntoItem";
 
-const PopoverList = ({ items, selectedIndex, onItemClick }) => {
-  const listRef = useRef(null);
-
-  useEffect(() => {
-    if (listRef.current && selectedIndex < items.length) {
-      listRef.current.children[selectedIndex].scrollIntoView({
-        behavior: "smooth",
-        block: "nearest",
-      })
-    }
-  }, [selectedIndex]);
+const PopoverList = ({ items, selectedItemIndex, onItemClick }) => {
+  const listRef = useScrollIntoItem(selectedItemIndex, items);
 
   return (
-    <ul ref={listRef} className="flex flex-col overflow-y-scroll overflow-x-hidden w-full h-3/4 focus-within:outline-none focus-within:border-none">
-      {
-        items.map((item, index) => (
-          <li
-            key={index}
-            onClick={() => onItemClick(index)}
-            className={twMerge("flex items-center justify-start gap-6 py-2 px-5 duration-150 ease-in hover:bg-gray-200 cursor-pointer", `${selectedIndex === index && "bg-gray-200"}`)}
-          >
-            <Pencil color="#000000" />
-            <div>
-              <p className="text-gray-700 font-bold text-lg">{item.format}</p>
-              <p className="text-gray-400 text-sm">Shortcut: type {item.shortcut}</p>
-            </div>
-          </li>
-        ))
-      }
-    </ul>
+    <div ref={listRef} className="bg-gray-50 dark:bg-[#303031]">
+      <div className="py-1 px-2 text-sm text-[#deddda]">
+        <span>Basic blocks</span>
+      </div>
+      <ul className="flex flex-col focus-within:outline-none focus-within:border-none">
+        {
+          items.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => onItemClick(index)}
+              className={twMerge("flex items-center justify-start gap-6 py-2 px-5 duration-150 ease-in hover:bg-gray-200 hover:dark:bg-[#EA861A] cursor-pointer", `${selectedItemIndex === index && "bg-gray-200 dark:bg-[#EA861A]"}`)}
+            >
+              <Pencil color="#000000" />
+              <div>
+                <p className="text-gray-700 dark:text-[#deddda] font-bold">{item.format}</p>
+                <p className="text-gray-400 dark:text-gray-100 text-xs">Shortcut: type {item.shortcut}</p>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 };
 

54e4847f6b67dbbdc5b78a55482d54c04c071488
kweeka <mtalbi29@yahoo.com>
2023-08-23 17:28:44 +0100

    Refactor: Move code to editor folder

--- src/components/ContentEditable.jsx
+++ src/components/ContentEditable.jsx
@@ -4,19 +4,6 @@ import { forwardRef } from "react";
 /* eslint-disable react/prop-types */
 const ContentEditable = forwardRef(({ onInput, placeholder, className }, ref) => {
 
-  const handleInput = (e) => {
-    const value = e.target.innerHTML;
-    (value === '<br>' || value === '<div><br></div>') && (e.target.innerHTML = "<br>");
-    onInput(e);
-  };
-
-
-  const pasteAsPlainText = (event) => {
-    event.preventDefault()
-    const text = event.clipboardData.getData('text/plain')
-    document.execCommand('insertHTML', false, text)
-  }
-
   return (
     <>
       <div
@@ -25,8 +12,7 @@ const ContentEditable = forwardRef(({ onInput, placeholder, className }, ref) =>
         className={twMerge('w-full text-slate-700 dark:text-[#deddda] text-base font-normal focus-within:outline-none whitespace-pre-wrap break-words before:text-slate-400 before:cursor-text empty:before:content-[attr(data-placeholder)]', className)}
         contentEditable
         suppressContentEditableWarning
-        onPaste={pasteAsPlainText}
-        onInput={handleInput}
+        onKeyDown={(e) => onInput(e)}
       >
         <br/>
       </div>

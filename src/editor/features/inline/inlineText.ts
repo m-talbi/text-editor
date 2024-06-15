f9a25b607a6fc14a84af255cdd78ff0cd95ecd18
Kweeka <mtalbi29@yahoo.com>
2024-06-15 13:36:48 +0100

    Fix spell mistake

--- src/editor/features/inline/inlineText.ts
+++ src/editor/features/inline/inlineText.ts
@@ -1,9 +1,9 @@
 const boldIcon =
   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bold"><path d="M14 12a4 4 0 0 0 0-8H6v8"/><path d="M15 20a4 4 0 0 0 0-8H6v8Z"/></svg>';
-const italticIcon =
-  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-italic"><line x1="19" x2="10" y1="4" y2="4"/><line x1="14" x2="5" y1="20" y2="20"/><line x1="15" x2="9" y1="4" y2="20"/></svg>'
+const italicIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-italic"><line x1="19" x2="10" y1="4" y2="4"/><line x1="14" x2="5" y1="20" y2="20"/><line x1="15" x2="9" y1="4" y2="20"/></svg>';
 
-  const inlineText = {
+const inlineText = {
   name: "Inline Text",
   formats: [
     {
@@ -15,9 +15,9 @@ const italticIcon =
       id: 1,
     },
     {
-      format: "Italtic",
+      format: "Italic",
       tag: "i",
-      icon: italticIcon,
+      icon: italicIcon,
       classes: ["italic"],
       shortcut: "* + space",
       id: 2,
